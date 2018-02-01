7cd9bd7b36f279c3c60d3b7cb31e90826e6f7857
YuliaD <34177068+YuliannkA@users.noreply.github.com>
2018-02-01 13:13:53 -0500

    Add files via upload

---
+++ SDSdemographicspractice.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+stats = pd.read_csv('C:\Users\Yulia\Desktop\My Data Science\Python\DemographicData.csv')
+stats.head()
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+#%matplotlib inline
+plt.rcParams['figure.figsize'] = 8,4
+
+vis1 = sns.distplot(stats["InternetUsers"])
+vis2 = sns.boxplot(data=stats, x="IncomeGroup",y="BirthRate")
+vis3 = sns.lmplot(data=stats, x="InternetUsers",y="BirthRate",fit_reg=False, hue='IncomeGroup', \
+                 scatter_kws={"s":100})
